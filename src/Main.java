f9ca4e0d3df6ec5a020d2b4d4ff3b403067e010c
MaraOD555 <ruffbc@hotmail.com>
2022-05-23 22:23:00 +0300

    Задача 1 - вывод ряда чисел в одну строку через пробел и на новой строке ряд чисел с уменьшением

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        //Выведите в одну строку через пробел числа от 1 до 10, используя цикл while.
+        //На следующей строке выведите числа в обратном порядке от 10 до 1, используя оператор for.
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        for (int a = 10; a >= 1; a--) {
+            System.out.print(a + " ");
+        }
     }
 }
