dbb2ff6af7bbd42a9cfe0a4dd8f0c2babd84c494
MaraOD555 <ruffbc@hotmail.com>
2022-05-24 13:31:55 +0300

    Задача 3 Года, когда комета пролетает рядом с Землей, до 200 лет и 100 лет вперед

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,22 @@ public class Main {
         //о том, что нужно подготовить еженедельный отчет.
         //Пятница отчетный день.
 
-        for (int dayNumber = 6; dayNumber <=31; dayNumber = dayNumber + 7) {
-             System.out.println("Сегодня пятница " + dayNumber + " число. Необходимо подготовить отчет");
+        for (int dayNumber = 6; dayNumber <= 31; dayNumber = dayNumber + 7) {
+            System.out.println("Сегодня пятница " + dayNumber + " число. Необходимо подготовить отчет");
+        }
+        System.out.println();
+        //Задание 3
+        //В консоль нужно вывести все годы за последние 200 лет, когда появлялась комета,
+        //а также следующий год ее появления.
+
+        int year1 = 2100;
+        int yearForComet = 0;
+        for (yearForComet = 0; yearForComet <= year1; yearForComet = yearForComet + 79) {
+            if (yearForComet >= 1800) {
+                System.out.println(yearForComet);
+
             }
         }
+    }
 
 }
